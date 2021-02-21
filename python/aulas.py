59a9236589f73810dea16ed91f58dcc861427639
Sumaito <007joaojpkl@gmail.com>
2021-02-21 17:06:46 -0300

    python test

---
+++ python/aulas.py
@@ -0,0 +1,4 @@
+um=input('Escolha o primeiro número, ou, se preferir números:')
+dois=input('Agora do mesmo jeite escolha um ou mais números:')
+três=input('Por fim iremos, juntar esses resultados, não iremos somar, por favor não digite nada só aperte ENTER')
+print(um+dois+três)
---
+++ python/exercise1.py
@@ -0,0 +1,9 @@
+#programming "Hello World" in Python 3 in two different ways
+#first shap
+
+print('Hello World')
+
+#second shape
+
+msg=('Hello World')
+print(msg)
