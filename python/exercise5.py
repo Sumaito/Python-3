af4ef1f1a6db31a1062740fbd61db46e7370d2cf
Sumaito <007joaojpkl@gmail.com>
2021-02-27 15:44:07 -0300

    teste

--- python/exercise5.py
+++ python/exercise5.py
@@ -1,3 +1,3 @@
 #Predecessor and successor to a number
-n=int(input('Digite um número: '))
-print('Verificando o valor de {}, o seu antecessor é {} e o seu sucessor é {}'.format(n,(n-1),(n+1)))
+n=int(input('Enter a number: '))
+print('Checking the value of {}, its predecessor is {} and its successor is {}'.format (n,(n-1),(n+1)))
--- python/exercise6.py
+++ python/exercise6.py
@@ -1,3 +1,3 @@
 #Code that calculates double, triple and square root
-n=int(input('Digite um número: '))
-print('O Dobro de {} é {}\nO triplo de {} é {}\nA raiz quadrada de {} é {:.2f}'.format(n,(n*2),n,(n*3),n,pow(n,(1/2))))
+n=int(input('Digit one number: '))
+print('The double of {} is {}\nThe triple of {} is {}\nThe square root of {} is {:.2f}'.format(n,(n*2),n,(n*3),n,pow(n,(1/2))))
